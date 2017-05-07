1db13e2e73ea81b26447221a6404df437f737134
harish <sharish@thoughtworks.com>
2017-05-07 16:57:11 +0530

    simple go routines to fetch stocks

--- main/main.go
+++ main/main.go
@@ -2,27 +2,48 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"encoding/json"
 	"time"
+	"runtime"
 )
 
 func main() {
 
-	godur,_ := time.ParseDuration("5ms")
+	companies := []string {"googl", "appl", "msft", "bbry", "hpq", "vz", "t", "tmus", "s"}
+	start := time.Now()
+	fetchedCompanies := 0
 
-	go func (){
-		for i := 0; i < 100; i++ {
-			fmt.Println("hello")
-			time.Sleep(godur)
-		}
-	}()
+	runtime.GOMAXPROCS(4)
 
-	go func (){
-		for i := 0; i < 100; i++ {
-			fmt.Println("go")
-			time.Sleep(godur)
-		}
-	}()
+	for _, company := range companies {
+		go func(company string) {
+			var share Share
+			resp, err := http.Get("http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=" + company);
 
-	dur,_ := time.ParseDuration("5s")
-	time.Sleep(dur)
+			if err != nil {
+				fmt.Println("failed unable to parse");
+				return
+			}
+			err = json.NewDecoder(resp.Body).Decode(&share)
+			if err != nil {
+				fmt.Println("unable to parse json", err)
+			}
+			fmt.Println("Symbol", company, "Company", share.Name, ": ", share.LastPrice)
+			fetchedCompanies++
+		}(company)
+	}
+
+	for fetchedCompanies < len(companies) {
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	elapsed := time.Since(start)
+	fmt.Println("time taken for above is", elapsed)
+}
+
+type Share struct{
+	Name string								`json:"Name"`
+	LastPrice float32					`json:"LastPrice"`
+	Symbol string							`json:"Symbol"`
 }
